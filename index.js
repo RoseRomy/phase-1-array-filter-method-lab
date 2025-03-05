4836598bb964fbac3bfb8e1849cc7d0ad71f8a93
+ <RoseKamau@Romys-Macboook-Air.local>
2025-03-05 10:55:06 +0300

    Addes index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(drivers => drivers.toLowerCase() === name.toLowerCase());
+}
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(drivers => drivers.startsWith(letters));
+}
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
